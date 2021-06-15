952cc78e8c0a1f22952ed711d9fca57320be4b4d
HanxiZhang9910 <linsu1702@gmail.com>
2021-06-15 18:03:25 -0400

    finished section 5

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const measureKevin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: '2',
+  };
+  console.table(measurement);
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kevin = measurement.value + 273;
+  return kevin;
+};
+
+console.log(measureKevin());
